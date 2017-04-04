d270ded890f4f72eb7fe62367a52fa72c5608bf8
danawacomputer <suns303@naver.com>
2017-04-04 11:07:28 +0900

    ㅇㅇ

---
+++ src/hello.java
@@ -0,0 +1,17 @@
+/**
+ * Created by danawacomputer on 2017-04-04.
+ */
+public class hello {
+    public static void main(String[] args) {
+
+        int busfee = 500;
+        int lunch = 5000;
+
+        int total = busfee+lunch;
+
+        System.out.println("토탈금액"+busfee+lunch);
+
+
+    }
+}
+
