1cae044542858307e6107b1ce4763f96fc802a47
danawacomputer <suns303@naver.com>
2017-04-04 11:09:52 +0900

    c

---
+++ src/My.java
@@ -0,0 +1,13 @@
+/**
+ * Created by danawacomputer on 2017-04-04.
+ */
+public class My {
+}    public static void main(String[] args) {
+
+    int busfee = 500;
+    int lunch = 5000;
+
+    int total = busfee+lunch;
+
+    System.out.println("토탈금액은"+busfee+lunch);
+
