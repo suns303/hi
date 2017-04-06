b4a6f94a771b49ff5ce1dd1a27986823fb1e2a5a
danawacomputer <suns303@naver.com>
2017-04-07 08:59:12 +0900

    arraylistexamplefuction

---
+++ src/ArrayListExamlpleFuction.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+/**
+ * Created by danawacomputer on 2017-04-06.
+ */
+public class ArrayListExamlpleFuction {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static double getAverageFromList(List<String> wordList){
+        //ToDo
+        return 0;
+    }
+
+    public static List<String> getListLessThanFive(List<String>wordList){
+    //ToDo
+        return null;
+    }
+}
