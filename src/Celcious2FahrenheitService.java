92ab4a63d0d463b956d85bca8da67b0df7fc9676
danawacomputer <suns303@naver.com>
2017-04-07 14:08:16 +0900

    cccaa

---
+++ src/Celcious2FahrenheitService.java
@@ -0,0 +1,9 @@
+/**
+ * Created by danawacomputer on 2017-04-07.
+ */
+public class Celcious2FahrenheitService {
+
+    public double toFaranheit(int celcius) {
+        return (celcius * 1.8) + 32;
+    }
+}
